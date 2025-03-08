74548aa19d86d80d3b924a1e3e29c9e7ca20cd1c
shivani joshi <161042809+749977@users.noreply.github.com>
2025-03-08 14:11:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-//your JS code here. If required.
+document.addEventListener("DOMContentLoaded", function () {
+    let form = document.getElementById("voteForm");
+
+    form.addEventListener("submit", function (e) {
+        e.preventDefault();
+        let name = document.getElementById("name").value.trim();
+        let age = parseInt(document.getElementById("age").value);
+
+        if (!name || isNaN(age)) {
+            alert("Please enter valid details.");
+            return;
+        }
+
+        let votingPromise = new Promise((resolve, reject) => {
+            setTimeout(() => {
+                age > 18 ? resolve(`Welcome, ${name}. You can vote.`) : reject(`Oh sorry ${name}. You aren't old enough.`);
+            }, 4000);
+        });
+
+        votingPromise
+            .then(message => alert(message))
+            .catch(error => alert(error));
+    });
+});
+
